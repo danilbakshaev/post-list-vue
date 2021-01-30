156820f614ebcb29d76b78185bb7162b69497f41
danilbakshaev <bakshaevdanil@gmail.com>
2021-01-30 17:29:42 +0500

    deploy

---
+++ js/chunk-2d0e5e97.6541cf7e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"center"},[t("h1",[e._v("Page Not Found")])])}],s=t("2877"),u={},a=Object(s["a"])(u,c,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0e5e97.6541cf7e.js.map
--- src/views/NotFound.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="center">
-    <h1>Page Not Found</h1>
-  </div>
-</template>
